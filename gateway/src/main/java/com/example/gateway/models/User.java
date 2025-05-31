f3733448dd0773ca4bf3e083a8222c520d92cdab
ak28sh <akshatakotti28@gmail.com>
2025-05-31 19:45:26 +0900

    add user model

---
+++ gateway/src/main/java/com/example/gateway/models/User.java
@@ -0,0 +1,13 @@
+package com.example.gateway.models;
+
+public class User {
+
+
+    private Long id;
+
+    private String username;
+    private String password;
+    private String email;
+    private String role;
+
+}
